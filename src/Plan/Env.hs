bc132ffdc45f44e811a70f08fff55b10f6cc7717
Se De <Sede7473@gmail.com>
2019-05-18 12:36:09 +0000

    Use Config for both [Task] and [Event]

--- src/Plan/Env.hs
+++ src/Plan/Env.hs
@@ -10,9 +10,13 @@ import Plan.Task.Type
 import RIO
 
 data Env = Env
+  { config :: Config
+  , situation :: Situation
+  }
+
+data Config = Config
   { tasks :: [Task]
   , events :: [Event]
-  , situation :: Situation
   }
 
 data Situation = Situation
@@ -28,20 +32,30 @@ newtype CurrentTime = CurrentTime
   { getTime :: UTCTime
   } deriving (Eq, Show, ToJSON, FromJSON)
 
-instance Has [Task] Env where
-  hasLens = lens tasks (\x y -> x {tasks = y})
-
-instance Has [Event] Env where
-  hasLens = lens events (\x y -> x {events = y})
-
 instance Has Situation Env where
   hasLens = lens situation (\x y -> x {situation = y})
+
+instance Has Config Env where
+  hasLens = lens config (\x y -> x {config = y})
+
+instance Has [Task] Config where
+  hasLens = lens tasks (\x y -> x {tasks = y})
+
+instance Has [Event] Config where
+  hasLens = lens events (\x y -> x {events = y})
 
 instance Has ConfigFile Situation where
   hasLens = lens configFile (\x y -> x {configFile = y})
 
 instance Has CurrentTime Situation where
   hasLens = lens currentTime (\x y -> x {currentTime = y})
+
+instance Has [Task] Env where
+  hasLens = lens (tasks . config) $ \x y -> x {config = (config x) {tasks = y}}
+
+instance Has [Event] Env where
+  hasLens =
+    lens (events . config) $ \x y -> x {config = (config x) {events = y}}
 
 instance Has ConfigFile Env where
   hasLens =
