5765b97f3f98ef18449f2635fbdc41b6dd1ba210
Se De <Sede7473@gmail.com>
2019-05-21 21:00:21 +0000

    Account for this in planning

--- src/Plan/Plan.hs
+++ src/Plan/Plan.hs
@@ -11,7 +11,7 @@ import Plan.Env
 import Plan.Task
 import Plan.TimeRange
 import Prelude (putStrLn)
-import RIO hiding ((^.), view, set)
+import RIO hiding ((^.), over, set, view)
 
 planDay ::
      (MonadReader a m, HasTime a UTCTime, HasTasks a [Task]) => m (Set Task)
@@ -23,7 +23,7 @@ planDay = do
       timeNow = timeToTimeOfDay t
       xs = sortOn (view importance) ts'
       f n ts =
-        case n ^. scheduled of 
+        case n ^. scheduled of
           Just e
             | e ^. start >= timeToTimeOfDay t -> insert n ts
             | n ^. deadline /= day -> ts
@@ -31,18 +31,24 @@ planDay = do
               flip insert ts $ set identifier 0 n
             | otherwise ->
               flip insert ts $
-              set
-                (scheduled . _Just . start)
-                (timeToTimeOfDay t) 
-                n
+              set (scheduled . _Just . start) (timeToTimeOfDay t) n
           Nothing ->
-            let need =
-                  diffTimeToPicoseconds (n ^. timeNeeded) `div`
-                  diffDays (n ^. deadline) day
+            let m =
+                  (n &) $
+                  over timeNeeded $
+                  subtract $
+                  sum $
+                  fmap timeRangeSize $
+                  mappend (n ^. workedToday) $
+                  maybe [] ((: []) . flip TimeRange (timeToTimeOfDay t)) $
+                  n ^. workingFrom
+                need =
+                  diffTimeToPicoseconds (m ^. timeNeeded) `div`
+                  diffDays (m ^. deadline) day
                 attemptInsert i
                   | i + 1 >= length ts = ts
                   | convert planEnd - convert planStart >= need =
-                    insert (set scheduled (Just plannedTimeRange) n) ts
+                    insert (set scheduled (Just plannedTimeRange) m) ts
                   | otherwise = attemptInsert (i + 1)
                   where
                     planStart =
@@ -51,16 +57,24 @@ planDay = do
                       view start $ fromJust $ view scheduled $ elemAt (i + 1) ts
                     convert = diffTimeToPicoseconds . timeOfDayToTime
                     plannedTimeRange =
-                      TimeRange planStart $ timeToTimeOfDay $
-                      timeOfDayToTime planStart +
-                      picosecondsToDiffTime need
+                      TimeRange planStart $
+                      timeToTimeOfDay $
+                      timeOfDayToTime planStart + picosecondsToDiffTime need
              in attemptInsert 0
   return $
     foldr
       f
       (fromList
          [ Task (Just $ TimeRange timeNow timeNow) 0 0 day "Now" 0 [] Nothing
-         , Task (Just $ TimeRange midnight' midnight') 0 0 day "Midnight" 0 [] Nothing
+         , Task
+             (Just $ TimeRange midnight' midnight')
+             0
+             0
+             day
+             "Midnight"
+             0
+             []
+             Nothing
          ])
       xs
 
@@ -72,7 +86,7 @@ printPlan ::
      , MonadIO m
      )
   => m ()
-printPlan = do
+printPlan
     {-
   mapM_ removeItem $ fmap (^. identifier) $
     filter
@@ -80,10 +94,9 @@ printPlan = do
     env ^.
     events
     -}
+ = do
   d <- planDay
   forM_ d $ \(Task (Just (TimeRange s e)) _ _ _ n i _ _) ->
     let f = take 5 . show
-     in unless (i == 0) $ liftIO $ putStrLn $ show i <> ") " <> f s <> "-" <>
-        f e <>
-        ": " <>
-        n
+     in unless (i == 0) $
+        liftIO $ putStrLn $ show i <> ") " <> f s <> "-" <> f e <> ": " <> n
