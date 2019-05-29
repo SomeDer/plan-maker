6df11cff29cdcda9bddb35137d76a515e0a40b87
Se De <Sede7473@gmail.com>
2019-05-29 12:02:21 +0000

    Add WithIndex class

--- test/Instances.hs
+++ test/Instances.hs
@@ -1,6 +1,7 @@
 module Instances where
 
 import Control.Applicative
+import Control.Lens.Getter
 import Control.Monad
 import Plan.Env
 import Plan.Functions
@@ -38,8 +39,19 @@ instance Arbitrary Config where
         f ts = do
           opt <- arbitrary
           s <- arbitrary
-          (_, Config ts' _) <-
-            runMonads' (addTask s opt) (conf ts) (env ts)
+          (_, Config ts' _) <- runMonads' (addTask s opt) (conf ts) (env ts)
           frequency [(1, return ts'), (10, f ts')]
     ts <- f []
     return $ Config ts d
+
+data WithIndex = WithIndex
+  { ident :: Int
+  , configuration :: Config
+  } deriving (Eq, Show)
+
+instance Arbitrary WithIndex where
+  arbitrary = do
+    c <- arbitrary
+    let l = length $ c ^. tasks
+    i <- elements [1 .. l]
+    return $ WithIndex i c
--- test/RemoveSpec.hs
+++ test/RemoveSpec.hs
@@ -3,7 +3,7 @@ module RemoveSpec where
 import Control.Lens
 import Data.Bool
 import Data.Either
-import Instances ()
+import Instances
 import Plan.Env
 import Plan.Functions
 import Test.Hspec
@@ -13,15 +13,26 @@ spec :: SpecWith ()
 spec =
   describe "Removing tasks" $ do
     it "fails for negative identifiers" $
-      property $ \(x, c, env) -> do
-        (r, _) <-
+      property $ \(x, c, s) -> do
+        (e, _) <-
           runMonads'
             (removeItem $ fromIntegral $ negate (bool x 1 $ (x :: Word) == 0))
             c
-            env
-        r `shouldSatisfy` isLeft
+            (Env c s)
+        e `shouldSatisfy` isLeft
     it "decreases amount of tasks by 1 for success, 0 for failure" $
-      property $ \(x, c, env) -> do
-        (e, c') <- runMonads' (removeItem $ fromIntegral (x :: Word)) c env
+      property $ \(x, c, s) -> do
+        (e, c') <-
+          runMonads' (removeItem $ fromIntegral (x :: Word)) c $ Env c s
         length (c' ^. tasks) `shouldBe` length (c ^. tasks) -
           bool 0 1 (isRight e)
+    it "decreases amount of tasks by 1 if identifier exists" $
+      property $ \(WithIndex x c, s) -> do
+        (_, c') <-
+          runMonads' (removeItem $ fromIntegral x) c $ Env c s
+        length (c' ^. tasks) `shouldBe` length (c ^. tasks) - 1
+    it "fails if there are no tasks" $
+      property $ \(x, s, d) -> do
+        (e, _) <-
+          runMonads' (removeItem x) (Config [] d) $ Env (Config [] d) s
+        e `shouldSatisfy` isLeft
