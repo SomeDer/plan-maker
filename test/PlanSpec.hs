2d25e4c99430997e2bb216cf4a36f36ed4a5e178
Se De <Sede7473@gmail.com>
2019-06-30 16:15:34 +0100

    Use IDs in tests (fixes tests)

--- test/PlanSpec.hs
+++ test/PlanSpec.hs
@@ -42,15 +42,15 @@ spec =
     it "prints simple tasks consecutively" $ do
       (Right r, _) <- runMonads' printPlan con1 $ Env con1 baseSit
       r `shouldBe`
-        "1) 15:00-16:00: C\n\
-        \1) 16:00-17:00: B\n\
+        "3) 15:00-16:00: C\n\
+        \2) 16:00-17:00: B\n\
         \1) 17:00-18:00: A"
     it "prints simple tasks in importance order" $ do
       (Right r, _) <- runMonads' printPlan con2 $ Env con2 baseSit
       r `shouldBe`
         "1) 15:00-16:00: A\n\
-        \1) 16:00-17:00: B\n\
-        \1) 17:00-18:00: C"
+        \2) 16:00-17:00: B\n\
+        \3) 17:00-18:00: C"
     it "gives 20 minutes for a 6 day, 2 hour task" $ do
       (Right r, _) <- runMonads' printPlan con3 $ Env con3 baseSit
       r `shouldBe` "1) 15:00-15:20: A"
@@ -68,7 +68,7 @@ spec =
       (Right r, _) <-
         runMonads' printPlan con5 $ Env con5 baseSit
       r `shouldBe`
-        "1) 15:00-16:00: B\n\
+        "2) 15:00-16:00: B\n\
         \Some tasks are finished for today:\n\
         \1) A"
     it "correctly updates for new days" $ do
